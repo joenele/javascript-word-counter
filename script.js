7b352800909cbf9b16108db78b448d07a9b7572d
joenele <joenele.young@gmail.com>
2022-03-14 13:46:49 -0400

    functions for word and character count

---
+++ script.js
@@ -0,0 +1,14 @@
+document
+.querySelector("#word")
+.addEventListener("input", function countWord() {
+    let res = [];
+    let str = this.value.replace(/[\t\n\r\.\?\!]/gm, " ").split(" ");
+    str.map((s) => {
+        let trimStr = s.trim() ;
+        if (trimStr.length > 0 ) {
+            res.push(trimStr);
+        }
+    });
+    document.querySelector("#words").innerText = res.length;
+    document.querySelector("#characters").innerText = char.length
+})
